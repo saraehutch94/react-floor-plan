3992b12fc3dd3a84efec8a131e904ea823d7127f
saraehutch94 <84148986+saraehutch94@users.noreply.github.com>
2021-12-28 19:01:41 -0500

    create Sink component, export to Kitchen

--- src/components/Kitchen.js
+++ src/components/Kitchen.js
@@ -1,10 +1,12 @@
 import Oven from "./Oven";
+import Sink from "./Sink";
 
 function Kitchen() {
   return (
     <div>
       <div>Kitchen</div>
       <Oven />
+      <Sink />
     </div>
   );
 }
